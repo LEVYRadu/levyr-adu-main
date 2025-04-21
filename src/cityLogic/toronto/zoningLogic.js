c62b78aff160e1e93aba8cdbe985d51cf126d998
LEVYRadu <levyr.adu@protonmail.com>
2025-04-21 14:34:54 -0400

    Create zoningLogic.js

---
+++ src/cityLogic/toronto/zoningLogic.js
@@ -0,0 +1,8 @@
+// Placeholder zoning logic for Toronto
+export default async function zoningLogic(coordinates) {
+  return {
+    isADUPermitted: true, // Pretend ADUs are always allowed for now
+    zoneCode: "R",        // Placeholder zoning code
+    notes: ["Placeholder logic: assuming ADU permitted in Toronto."],
+  };
+}
