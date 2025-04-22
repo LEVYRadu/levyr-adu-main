af5f2bf763a5fa46bacddd36fbb3e646a6ffe0c3
LEVYRadu <levyr.adu@protonmail.com>
2025-04-21 20:58:19 -0400

    Create ontarioShared.js

---
+++ src/cityLogic/ontarioShared.js
@@ -0,0 +1,42 @@
+// Ontario-wide shared logic
+import turf from "@turf/turf"; // assuming you add this to project later
+
+// Fetch and cache the datasets once
+let greenbeltFeatures = null;
+let soilFeatures = null;
+let elevationFeatures = null;
+
+async function fetchAndCacheGeoJSON(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.features;
+}
+
+export async function loadOntarioLayers() {
+  if (!greenbeltFeatures) {
+    greenbeltFeatures = await fetchAndCacheGeoJSON("https://ws.lioservices.lrc.gov.on.ca/arcgis2/rest/services/LIO_OPEN_DATA/LIO_Open01/MapServer/29/query?outFields=*&where=1%3D1&f=geojson");
+  }
+  if (!soilFeatures) {
+    soilFeatures = await fetchAndCacheGeoJSON("https://ws.lioservices.lrc.gov.on.ca/arcgis2/rest/services/LIO_OPEN_DATA/LIO_Open06/MapServer/15/query?outFields=*&where=1%3D1&f=geojson");
+  }
+  if (!elevationFeatures) {
+    elevationFeatures = await fetchAndCacheGeoJSON("https://ws.lioservices.lrc.gov.on.ca/arcgis2/rest/services/LIO_OPEN_DATA/LIO_Open05/MapServer/9/query?outFields=*&where=1%3D1&f=geojson");
+  }
+}
+
+export function isInGreenbelt(coordinates) {
+  const point = turf.point([coordinates.lng, coordinates.lat]);
+  return greenbeltFeatures?.some(feature => turf.booleanPointInPolygon(point, feature));
+}
+
+export function getSoilType(coordinates) {
+  const point = turf.point([coordinates.lng, coordinates.lat]);
+  const match = soilFeatures?.find(feature => turf.booleanPointInPolygon(point, feature));
+  return match?.properties?.SOIL_NAME || "Unknown";
+}
+
+export function getElevation(coordinates) {
+  const point = turf.point([coordinates.lng, coordinates.lat]);
+  const match = elevationFeatures?.find(feature => turf.booleanPointInPolygon(point, feature));
+  return match?.properties?.ELEVATION || "Unknown";
+}
