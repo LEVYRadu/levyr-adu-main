d299a51277d96fec44cc5d66ce5408690f0d1d29
LEVYRadu <levyr.adu@protonmail.com>
2025-04-22 10:17:00 -0400

    fix

--- src/cityLogic/cityRouter.js
+++ src/cityLogic/cityRouter.js
@@ -1,20 +1,20 @@
-import validateCity from '../utils/validateCity'; // Import validateCity
+import validateCity from '../utils/validateCity';
 import hamilton from './hamilton';
 import toronto from './toronto';
 
-const cityRouter = async (city) => {
+const cityRouter = (city) => {
   if (!validateCity(city)) {
     return { error: 'City not supported' };
   }
 
   switch (city) {
     case 'Hamilton':
-      return await hamilton(); // Await the hamilton function
+      return hamilton;
     case 'Toronto':
-      return await toronto();  // Await the toronto function
+      return toronto;
     default:
       return { error: 'City not supported' };
   }
 };
 
-export default cityRouter; // Ensure export is included
+export default cityRouter;
